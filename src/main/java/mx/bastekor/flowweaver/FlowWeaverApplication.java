3f1734902842cfd554492ee66977adcdf1d1e917
LEDZ <luiserik.diaz@gmail.com>
2025-05-09 23:05:02 -0600

    Incio de proyecto

---
+++ src/main/java/mx/bastekor/flowweaver/FlowWeaverApplication.java
@@ -0,0 +1,13 @@
+package mx.bastekor.flowweaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowWeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowWeaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/bastekor/flowweaver/FlowWeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.bastekor.flowweaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowWeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
